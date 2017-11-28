80098241056781c7b1502132d95d42a58814b24e
Dennis Qian <orangemoney52@gmail.com>
2017-11-28 15:41:20 -0800

    fixed borders

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-var x = 0;
-var y = 0;
-var lastScrollTop = 0;
-//var projectSection = document.querySelector('.project-pages');
-//var projectIndex = document.querySelector('.project-index');
-var sideBorders = document.querySelector('.project-index');
-var launchedProject = 0;
-var launchedProjectImg = 0;
-
 // Parallax Items Scrolling
 
 var PARALLAX_CONFIG = {
@@ -55,11 +46,9 @@ $(window).scroll();
 
 // Launch Project
 
-var projectSection = document.querySelector('.project-pages');
 var projectIndex = document.querySelector('.project-index');
 var launchProject = function(projectId) {
     var $projectPage = $('.project-page[data-project=' + projectId + ']');
-    console.log($projectPage);
 
     // hide project index
     $(projectIndex).removeClass("fadein").addClass("fadeout");
@@ -67,38 +56,6 @@ var launchProject = function(projectId) {
 
     // reveal project page
     $projectPage.removeClass("hide").addClass("reveal");
-    return;
-
-
-    $(projectIndex).addClass("hidden");
-
-    setTimeout(function() {
-$('.project-image').css({"transform" : "scale(1.4)"});
-$('.project-image').velocity({ "scale" : "1"}, {duration: 500});
-      $('.project-top').velocity({opacity: "1"}, {duration: 1000});
-
-  setTimeout(function() {
-    $('.close').addClass("visible");
-  }, 1000);
-
-  $(projectSection).removeClass("hidden");
-  $(el).addClass("launched");
-
-
-  $('.project-page').removeClass("off").addClass("on");
-  $('.project-page').velocity({
-    width: "100vw"
-  }, {
-    duration: 100,
-    easing: "easeOutCubic"
-  });
-  }, 1000);
-  setTimeout(function() {
-    $('.close').addClass("visible");
-    $('.cover').velocity({paddingTop: "0px"}, {duration: 200});
-      $(projectIndex).removeClass("hidden");
-  }, 2000);
-
 };
 
 var closeProject = function(projectId) {
@@ -110,7 +67,6 @@ var closeProject = function(projectId) {
 
     // reveal project page
     $projectPage.removeClass("reveal").addClass("hide");
-    return;
 };
 
 
@@ -123,6 +79,8 @@ $('.project').click(function(e) {
 $('.project-page .close').click(function(e) {
     closeProject($(this).parents('.project-page').data("project"));
 });
+
+// Misc
 
 function closeProject(el) {
     $(projectSection).addClass("hidden");
@@ -174,25 +132,12 @@ function closeProject(el) {
     $('.cover').css({paddingTop: 0});
     $(projectSection).addClass("hidden");
   }, 1000);
-
-
-
 }
-$(function() {
-  $('.pro').hover(function() {
-    $('.top-border').css('height', '12px');
-    $('.bottom-border').css('height', '12px');
-    $('.right-border').css('width', '12px');
-    $('.left-border').css('width', '12px');
-      $(this).find('.main-title').addClass("flipped");
-    $(this).find('.project-title-mask').css('height', '20px');
-  }, function() {
-    // on mouseout, reset the background colour
-    $('.top-border').css('height', '0px');
-    $('.bottom-border').css('height', '0px');
-    $('.right-border').css('width', '0px');
-    $('.left-border').css('width', '0px');
-      $(this).find('.main-title').removeClass("flipped");
-      $(this).find('.project-title-mask').css('height', '0px');
-  });
+
+// Hover Border Reveal
+
+$('.project').hover(function() {
+    $(projectIndex).addClass('border-reveal');
+}, function() {
+    $(projectIndex).removeClass('border-reveal');
 });

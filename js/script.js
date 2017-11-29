130d1a349b65244ad3e2ac00e33f6479fbeffa0b
Dennis Qian <orangemoney52@gmail.com>
2017-11-28 17:06:40 -0800

    WE DID THE MATH

--- js/script.js
+++ js/script.js
@@ -22,17 +22,17 @@ var parallaxScroll = function(e) {
 
         if (pagePosition <= centeredOffset - PARALLAX_CONFIG.verticalBoundSize) {
             $(this).css({
-                transform: "translate(0, " + PARALLAX_CONFIG.translateDistance + "px)",
+                transform: "translate3d(0, " + PARALLAX_CONFIG.translateDistance + "px, 0)",
                 transition: "transform " + PARALLAX_CONFIG.outgoingTranslateTiming + "s",
             });
         } else if (pagePosition > centeredOffset - PARALLAX_CONFIG.verticalBoundSize && pagePosition < centeredOffset + PARALLAX_CONFIG.verticalBoundSize) {
             $(this).css({
-                transform: "translate(0, 0)",
+                transform: "translate3d(0, 0, 0)",
                 transition: "transform " + PARALLAX_CONFIG.incomingTranslateTiming + "s",
             });
         } else {
             $(this).css({
-                transform: "translate(0, -" + PARALLAX_CONFIG.translateDistance + "px)",
+                transform: "translate3d(0, -" + PARALLAX_CONFIG.translateDistance + "px, 0)",
                 transition: "transform " + PARALLAX_CONFIG.outgoingTranslateTiming + "s",
             });
         };
@@ -56,6 +56,19 @@ var launchProject = function(projectId) {
 
     // reveal project page
     $projectPage.removeClass("hide").addClass("reveal");
+
+    // scale image from project index
+    setTimeout(function() {
+        var $projectImageFromIndex = $('.project[data-project-id=' + projectId + '] .project-image');
+        var scaleX = $(window).width() / $projectImageFromIndex.width();
+        var scaleY = $(window).height() / $projectImageFromIndex.height();
+        var offsetX = ($projectImageFromIndex.offset().left - ($(window).width() - $projectImageFromIndex.width()) / 2) / scaleX;
+        var offsetY = ($projectImageFromIndex.offset().top - $(window).scrollTop() - ($(window).height() - $projectImageFromIndex.height()) / 2) / scaleY;
+        $projectImageFromIndex.css({
+            transform: "scale3d(" + scaleX + ", " + scaleY + ", 1) translate3d(" + -offsetX + "px," + -offsetY + "px,0)", //  translate3d(" + -offsetX + "px," + -offsetY + "px,0)
+            transition: '',
+        });
+    }, 500); // same duration as fadein/fadeout transition
 };
 
 var closeProject = function(projectId) {
@@ -65,7 +78,14 @@ var closeProject = function(projectId) {
     $(projectIndex).removeClass("fadeout").addClass("fadein");
     $('body').removeClass('backgrounded');
 
-    // reveal project page
+    // scale image from project index
+    var $projectImageFromIndex = $('.project[data-project-id=' + projectId + '] .project-image');
+    $projectImageFromIndex.css({
+        transform: "",
+        transition: "transform .5s ease-in",
+    });
+
+    // hide project page
     $projectPage.removeClass("reveal").addClass("hide");
 };
 
@@ -82,57 +102,57 @@ $('.project-page .close').click(function(e) {
 
 // Misc
 
-function closeProject(el) {
-    $(projectSection).addClass("hidden");
+// function closeProject(el) {
+//     $(projectSection).addClass("hidden");
 
-    return;
+//     return;
 
-  launchedProject = document.querySelector('.launched');
-  launchedProjectImg = document.querySelector('.launched .back');
-  var domRect = launchedProject.getBoundingClientRect();
-  var domRectImg = launchedProjectImg.getBoundingClientRect();
+//   launchedProject = document.querySelector('.launched');
+//   launchedProjectImg = document.querySelector('.launched .back');
+//   var domRect = launchedProject.getBoundingClientRect();
+//   var domRectImg = launchedProjectImg.getBoundingClientRect();
 
-  //Get the offset of the square
-  var spaceBelow = domRect.bottom - window.scrollY + y + "px";
-  var spaceAbove = domRect.top - window.scrollY + y + "px";
-  var spaceLeft = domRect.left + "px";
-  var spaceRight = domRect.right + "px";
+//   //Get the offset of the square
+//   var spaceBelow = domRect.bottom - window.scrollY + y + "px";
+//   var spaceAbove = domRect.top - window.scrollY + y + "px";
+//   var spaceLeft = domRect.left + "px";
+//   var spaceRight = domRect.right + "px";
 
-  //Get the offset of the square's image
+//   //Get the offset of the square's image
 
-  var spaceBelowImg = domRectImg.bottom - window.scrollY + y + "px";
-  var spaceAboveImg = domRectImg.top - window.scrollY + y + "px";
-  var spaceLeftImg = domRectImg.left + "px";
-  var spaceRightImg = domRectImg.right + "px";
+//   var spaceBelowImg = domRectImg.bottom - window.scrollY + y + "px";
+//   var spaceAboveImg = domRectImg.top - window.scrollY + y + "px";
+//   var spaceLeftImg = domRectImg.left + "px";
+//   var spaceRightImg = domRectImg.right + "px";
 
-  //Resize project-image to the size of square's image
-
-
-  $('.project-image').css({marginLeft: spaceLeftImg, marginTop: spaceAboveImg});
-  $('.project-image').removeClass("full").addClass("shrink");
-
-  //Hide the Close icon
-
-  $('.close').removeClass("visible").addClass("hidden");
-  $('.cover').velocity({paddingTop: "400px"}, {duration: 200, easing: "easeOutCubic"});
-
-  // Clip project page to the size of the square
-
-  $('.project-page').css({
-    clipPath: "polygon(" + spaceLeft + " " + spaceAbove + ", " + spaceRight + " " + spaceAbove + ", " + spaceRight + " " + spaceBelow + ", " + spaceLeft + " " + spaceBelow + " )"
-  });
-    $(launchedProject).removeClass("launched");
-
-  // After Animation is complete, reset all properties
+//   //Resize project-image to the size of square's image
 
 
-  setTimeout(function() {
-    $('.project-page').removeAttr('style').removeClass("on").addClass("off");
-    $('.project-image').removeAttr('style').removeClass("shrink").addClass("full");
-    $('.cover').css({paddingTop: 0});
-    $(projectSection).addClass("hidden");
-  }, 1000);
-}
+//   $('.project-image').css({marginLeft: spaceLeftImg, marginTop: spaceAboveImg});
+//   $('.project-image').removeClass("full").addClass("shrink");
+
+//   //Hide the Close icon
+
+//   $('.close').removeClass("visible").addClass("hidden");
+//   $('.cover').velocity({paddingTop: "400px"}, {duration: 200, easing: "easeOutCubic"});
+
+//   // Clip project page to the size of the square
+
+//   $('.project-page').css({
+//     clipPath: "polygon(" + spaceLeft + " " + spaceAbove + ", " + spaceRight + " " + spaceAbove + ", " + spaceRight + " " + spaceBelow + ", " + spaceLeft + " " + spaceBelow + " )"
+//   });
+//     $(launchedProject).removeClass("launched");
+
+//   // After Animation is complete, reset all properties
+
+
+//   setTimeout(function() {
+//     $('.project-page').removeAttr('style').removeClass("on").addClass("off");
+//     $('.project-image').removeAttr('style').removeClass("shrink").addClass("full");
+//     $('.cover').css({paddingTop: 0});
+//     $(projectSection).addClass("hidden");
+//   }, 1000);
+// }
 
 // Hover Border Reveal
 
